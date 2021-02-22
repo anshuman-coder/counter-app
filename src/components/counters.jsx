3b7639bf16ed41a7e2b869b937d1203021b11cd4
unknown <anshurockingboy23@gmail.com>
2021-02-22 13:40:10 +0530

    updating state till now

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 import Counter from './counter';
 
 
-
-
-
 class Counters extends Component {
     state = { 
         counter: [
@@ -14,12 +11,21 @@ class Counters extends Component {
             { id: 4, value: 0},
         ]
      };
+    
+    handleReset= () => {
+      const counters =  this.state.counter.map(c => {c.value = 0;
+        return c;
+    });
+    this.setState({counter: counters});
+    };
+    
     handleDelete = counterId => {
         const counters = this.state.counter.filter(c => c.id !== counterId);
         this.setState({counter: counters});
     };
     render() { 
         return( <div> 
+            <button onClick={this.handleReset} className="brn btn-primary btn-sm m-2"> Reset </button>
             { this.state.counter.map(counter => 
             <Counter key={counter.id} onDelete={this.handleDelete} counter={counter}>
             </Counter>) }
